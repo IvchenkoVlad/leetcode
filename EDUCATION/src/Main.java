b57276d2dbfea2d0a40aff3bff9f00bb818909e6
IvchenkoVlad <ivchenko.vladyslav@gmail.com>
2022-02-23 17:53:34 -0500

    edu

---
+++ EDUCATION/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int switchValue = 1;
+        switch (switchValue) {
+            case 1:
+                System.out.println("hello");
+        }
+    }
+}
